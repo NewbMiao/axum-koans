2a9f0a4173c9305b2c7e277981fb1d0543109e97
newbmiao <newbvirgil@gmail.com>
2023-06-13 18:27:36 +0800

    fix ci

--- infrastructure/services-up.sh
+++ infrastructure/services-up.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-docker-compose up -d
-sqlx migrate run --database-url "postgresql://axum-koans:axum-koans-sec@localhost:15432/axum-koans"
+set -e
+workspace=$(cd "$(dirname "$0")" && pwd -P)
+{
+    cd "$workspace"
+    docker-compose up -d
+    cd -
+    sqlx migrate run --database-url "postgresql://axum-koans:axum-koans-sec@localhost:15432/axum-koans"
+}
