a2266cd937cd9ddf6cb0b5bac6ce1cb50a0ef9f3
newbmiao <newbvirgil@gmail.com>
2023-06-26 17:34:45 +0800

    add test for introspect token

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -17,3 +17,45 @@ pub async fn user_handler(
         .await?;
     Ok(Json(res))
 }
+
+#[cfg(test)]
+mod test {
+    use std::sync::Arc;
+
+    use axum::{routing::get, Extension, Router};
+    use hyper::{http::HeaderValue, Request, StatusCode};
+    use tower::{ServiceBuilder, ServiceExt};
+
+    use crate::{
+        config::Config, errors::ServerError, extensions::keycloak_auth::KeycloakAuth,
+        handlers::user::user_handler,
+    };
+
+    #[tokio::test]
+    async fn test_log_request_response() -> Result<(), ServerError> {
+        let req = Request::builder()
+            .method("GET")
+            .uri("/")
+            .header(
+                "Authorization",
+                HeaderValue::from_static("Bearer test.test.test"),
+            )
+            .body(hyper::Body::empty())
+            .unwrap();
+
+        let config = Config::from_env()?;
+        // create a simple router to test the middleware
+        let app = Router::new().route("/", get(user_handler)).layer(
+            ServiceBuilder::new()
+                .layer(Extension(Arc::new(KeycloakAuth::new(config.keycloak))))
+                .into_inner(),
+        );
+
+        // send the request through the middleware
+        let res = app.clone().oneshot(req).await.unwrap();
+
+        // make sure the response has a status code of 200
+        assert_eq!(res.status(), StatusCode::UNAUTHORIZED);
+        Ok(())
+    }
+}
